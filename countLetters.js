737d2db672b0055065bb9b9a4aa04c6fd47a3b11
JIAQI13 <jiaqi11@ualberta.ca>
2021-04-28 22:04:55 +0000

    countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,23 +1,23 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`🤙Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`💩💩💩💩💩💩Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-
-  const countletters = function (sentense) {
-    let result = {};
-    for (const letter of sentense.toLowerCase()){
-        if (letter === ' '){
-            //pass
-        }else if (result[letter]){
-            result[letter] ++;
-        }else{
-            result[letter] = 1;
-        }
-    }
-    return result;
+  if (actual === expected) {
+    console.log(`🤙Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💩💩💩💩💩💩Assertion Failed: ${actual} !== ${expected}`);
   }
+};
+
+const countletters = function(sentense) {
+  let result = {};
+  for (const letter of sentense.toLowerCase()) {
+    if (letter === ' ') {
+      //pass
+    } else if (result[letter]) {
+      result[letter] ++;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
 
 console.log(countletters("Lighthouse in the house"));
